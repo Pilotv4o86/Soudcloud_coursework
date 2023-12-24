4f23fd2e3713018e9c2f60a39aee1179a6231fee
Podlina <polinka92668@gmail.com>
2023-12-24 19:55:19 +0300

    Изменены надписи на кнопках, фоновое изображение, размер окна приложения

--- src/main/java/GUI/AudioPlayerGUI.java
+++ src/main/java/GUI/AudioPlayerGUI.java
@@ -1,11 +1,12 @@
+
 package GUI;
 
 import Server.AudioPlayer;
 
 import javax.swing.*;
-
 import java.awt.*;
 import java.io.File;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +17,8 @@ public class AudioPlayerGUI {
     private JButton nextButton;
     private JButton prevButton;
     private JButton addTrackButton;
-    private JSlider progressBar;
-    private JLabel timeLabel;
-    private JList<File> playlistList; // Добавлен JList для отображения плейлиста
-    private DefaultListModel<File> playlistModel; // Модель для JList
-
+    private JList<File> playlistList;
+    private DefaultListModel<File> playlistModel;
 
     public AudioPlayerGUI() {
         DefaultListModel<File> playlistModel = new DefaultListModel<>();
@@ -29,73 +27,55 @@ public class AudioPlayerGUI {
     }
 
 
-
     private void createAndShowGUI() {
         JFrame frame = new JFrame("Audio Player");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
+        // Используем кастомный класс BackgroundPanel
+        BackgroundPanel panel = new BackgroundPanel();
+        panel.setLayout(new FlowLayout());
 
-        startStopButton = new JButton("Start");
+        startStopButton = new JButton("<html>&#9658;</html>"); // Это Unicode-символ для ▶
+        startStopButton.setFont(new Font("Arial", Font.PLAIN, 16)); // Вы можете настроить шрифт по вашему вкусу
         startStopButton.addActionListener(e -> handleStartStopButton());
 
-        nextButton = new JButton("Next");
+        nextButton = new JButton("<html>&#9197;</html>");
         nextButton.addActionListener(e -> audioPlayer.playNext());
 
-        prevButton = new JButton("Previous");
+        prevButton = new JButton("<html>&#9198;</html>");
         prevButton.addActionListener(e -> audioPlayer.playPrevious());
 
-        addTrackButton = new JButton("Add Track");
+        addTrackButton = new JButton("<html>&#10010;&#9836;</html>");
         addTrackButton.addActionListener(e -> handleAddTrackButton());
-
-        progressBar = new JSlider();
-        progressBar.setMinimum(0);
-        progressBar.setMaximum(100);
-        progressBar.setValue(0);
-        progressBar.addChangeListener(e -> {
-            if (!progressBar.getValueIsAdjusting()) {
-                int value = progressBar.getValue();
-               // audioPlayer.seek(value);
-            }
-        });
-
-        timeLabel = new JLabel("0:00");
 
         playlistModel = new DefaultListModel<>();
         playlistList = new JList<>(playlistModel);
         JScrollPane playlistScrollPane = new JScrollPane(playlistList);
-        playlistScrollPane.setPreferredSize(new Dimension(200, 150));
+        playlistScrollPane.setBounds(50,70,400,100);
+        frame.add(playlistScrollPane);
 
-        panel.add(playlistScrollPane);
-
-        // Обновите часть создания кнопок в createAndShowGUI
-        JButton searchButton = new JButton("Поиск");
+        JButton searchButton = new JButton("\uD83D\uDD0E");
         searchButton.addActionListener(e -> handleSearch());
         panel.add(searchButton);
 
-
-        panel.add(startStopButton);
         panel.add(prevButton);
+        panel.add(startStopButton);
         panel.add(nextButton);
         panel.add(addTrackButton);
-        panel.add(progressBar);
-        panel.add(timeLabel);
-
 
         frame.getContentPane().add(panel);
-        frame.setSize(400, 200);
+        frame.setSize(500, 400);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
-    private void handleStartStopButton()
-    {
+
+    private void handleStartStopButton() {
         if (audioPlayer.isPlaying()) {
             audioPlayer.stop();
-            startStopButton.setText("Start");
-        }
-        else {
+            startStopButton.setText("<html>&#9658;</html>");
+        } else {
             audioPlayer.play();
-            startStopButton.setText("Stop");
+            startStopButton.setText("<html>&#9209;</html>");
         }
     }
 
@@ -106,13 +86,12 @@ public class AudioPlayerGUI {
         if (result == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
             audioPlayer.addToPlaylist(selectedFile);
-            playlistModel.addElement(selectedFile); // Добавление трека в модель списка
+            playlistModel.addElement(selectedFile);
             JOptionPane.showMessageDialog(null, "Track added to the playlist: " + selectedFile.getName());
         }
     }
 
     private void handleSearch() {
-        // Диалоговое окно для ввода названия трека
         String searchTerm = JOptionPane.showInputDialog(null, "Введите название трека для поиска:");
 
         if (searchTerm != null && !searchTerm.isEmpty()) {
@@ -121,7 +100,6 @@ public class AudioPlayerGUI {
             if (matchingTracks.isEmpty()) {
                 JOptionPane.showMessageDialog(null, "Совпадений не найдено.");
             } else {
-                // Отобразите или выполните другие операции с найденными треками
                 for (File track : matchingTracks) {
                     System.out.println("Найден совпадающий трек: " + track.getName());
                 }
@@ -134,7 +112,6 @@ public class AudioPlayerGUI {
     private List<File> searchTracksByName(String searchTerm) {
         List<File> matchingTracks = new ArrayList<>();
 
-        // Проход по плейлисту и добавление соответствующих треков
         for (File track : audioPlayer.getPlaylist()) {
             if (track.getName().toLowerCase().contains(searchTerm.toLowerCase())) {
                 matchingTracks.add(track);
@@ -146,5 +123,29 @@ public class AudioPlayerGUI {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new AudioPlayerGUI());
+    }
+}
+
+// Кастомный класс BackgroundPanel
+class BackgroundPanel extends JPanel {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        File imageURL = new File(".\\src\\resources\\back1.gif");
+
+        if (imageURL != null) {
+            ImageIcon imageIcon = null;
+            try {
+                imageIcon = new ImageIcon(imageURL.toURL());
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+            Image image = imageIcon.getImage();
+            g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+        } else {
+            System.err.println("Image not found!");
+        }
     }
 }
